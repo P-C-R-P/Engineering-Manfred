6f5ab23e199291ad79c2eac65c50e71e8ecea8fb
ftorresc <32510211+ftorresc@users.noreply.github.com>
2022-01-24 16:18:40 +0100

    html rendering

--- buildxml.py
+++ buildxml.py
@@ -1,79 +1,94 @@
 import encodings
 import xml.etree.ElementTree as ET
+import lxml.etree as ET
 import re
 import fileinput
 import os
 
 
-inputFile = 'files/manfred_extracted.txt'
-xmlFile = 'files/output.xml'
+# inputFile = 'files/manfred_extracted.txt'
+# xmlFile = 'files/output.xml'
 
-tree = ET.ElementTree(element= ET.Element('document'))
-root=tree.getroot()
+# tree = ET.ElementTree(element= ET.Element('document'))
+# root=tree.getroot()
 
-# Add header
-header = ET.Element('header')
-root.append(header)
+# # Add header
+# header = ET.Element('header')
+# root.append(header)
 
-title = ET.Element('title')
-title.text = 'Manfred - Ingénierie Documentaire (2021)'
-header.append(title)
+# title = ET.Element('title')
+# title.text = 'Manfred - Ingénierie Documentaire (2021)'
+# header.append(title)
 
-authors = ET.Element('authors')
-authors.text = "compiled by Philipa Payne et Fábio Cabral"
-header.append(authors)
+# authors = ET.Element('authors')
+# authors.text = "compiled by Philipa Payne et Fábio Cabral"
+# header.append(authors)
 
-drama = ET.Element('drama')
-root.append(drama)
+# drama = ET.Element('drama')
+# root.append(drama)
 
-countActs = 0
-countScenes = 0
+# countActs = 0
+# countScenes = 0
 
-with open (inputFile, 'r', encoding="utf-8") as txt_file:
-    for line in txt_file:
-        #act match
-        a = re.match(r'\s*(ACT\s([I]{1,3})\.)', line)
-        if a:
-            countScenes = 0
-            countActs += 1
-            #print("Act caught")
-            act = ET.SubElement(drama, 'act', attrib = {'n': str(countActs)})
-            act.text = a.group()
+# with open (inputFile, 'r', encoding="utf-8") as txt_file:
+#     for line in txt_file:
+#         #act match
+#         a = re.match(r'\s*(ACT\s([I]{1,3})\.)', line)
+#         if a:
+#             countScenes = 0
+#             countActs += 1
+#             #print("Act caught")
+#             act = ET.SubElement(drama, 'act', attrib = {'n': str(countActs)})
+#             act.text = a.group()
 
-        b = re.match(r'\s*(SCENE\s([I]{1,3}V?)\.)', line)
-        if b:
-            countScenes += 1
-            #print("Scene caught")
-            scene = ET.SubElement(act, 'scene', attrib = {'n': str(countScenes)})
-            scene.text = b.group()
+#         b = re.match(r'\s*(SCENE\s([I]{1,3}V?)\.)', line)
+#         if b:
+#             countScenes += 1
+#             #print("Scene caught")
+#             scene = ET.SubElement(act, 'scene', attrib = {'n': str(countScenes)})
+#             scene.text = b.group()
 
-        c = re.match(r'\s* ([A-Z]*)\.', line)
-        if c:
-            #print("Character caught")
-            speaker = ET.SubElement(scene, 'speaker', attrib = {'name': c.group(1)})
-            speaker.text = c.group()
+#         c = re.match(r'\s* ([A-Z]*)\.', line)
+#         if c:
+#             #print("Character caught")
+#             speaker = ET.SubElement(scene, 'speaker', attrib = {'name': c.group(1)})
+#             speaker.text = c.group()
 
-        d = re.match(r'\s* ([A-Z]* [A-Z]*)\.', line)
-        if d:
-            #print("Two-part character caught")
-            speaker = ET.SubElement(scene, 'speaker', attrib = {'name': d.group(1)})
-            speaker.text = d.group()
-        e = re.match(r'\s*(\[[a-zA-Z \.\s,-]*\])', line)
-        if e:
-            print("Stage direction caught")
-            stage = ET.SubElement(scene, 'stage')
-            stage.text = e.group()
+#         d = re.match(r'\s* ([A-Z]* [A-Z]*)\.', line)
+#         if d:
+#             #print("Two-part character caught")
+#             speaker = ET.SubElement(scene, 'speaker', attrib = {'name': d.group(1)})
+#             speaker.text = d.group()
+#         e = re.match(r'\s*(\[[a-zA-Z \.\s,-]*\])', line)
+#         if e:
+#             print("Stage direction caught")
+#             stage = ET.SubElement(scene, 'stage')
+#             stage.text = e.group()
             
-        f = re.match(r'(.+)', line)
-        if f and not a and not b and not c and not d and not e:
-            print("Speech" + f.group())
-            speech = ET.SubElement(scene, 'speech')
-            speech.text = line
-        else:
-            pass
+#         f = re.match(r'(.+)', line)
+#         if f and not a and not b and not c and not d and not e:
+#             print("Speech" + f.group())
+#             speech = ET.SubElement(scene, 'speech')
+#             speech.text = line
+#         else:
+#             pass
 
-# regex for one part characters:" [A-Z]*\." 
-# regex for two part characters:" [A-Z]* [A-Z]*\."
+# # regex for one part characters:" [A-Z]*\." 
+# # regex for two part characters:" [A-Z]* [A-Z]*\."
 
-ET.indent(tree)
-tree.write(xmlFile, encoding="utf-8", xml_declaration=True)
+# ET.indent(tree)
+# tree.write(xmlFile, encoding="utf-8", xml_declaration=True)
+
+# HTML rendering
+
+xmlFile = 'files/output.xml'
+xsltFile = 'files/stylesheet.xslt'
+htmlFile = 'files/manfred.html'
+
+xml = ET.parse(xmlFile)
+xslt = ET.parse(xsltFile)
+
+transform = ET.XSLT(xslt)
+newdom = transform(xml)
+
+newdom.write(htmlFile, encoding="utf-8")
---
+++ files/stylesheet.xslt
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+     <xsl:template match="/">
+        <html>
+            <head>
+                <title>Manfred by George Gordon Byron</title>
+            </head>
+            <body>
+                <h2>Compiled by Philipa Payne and Fábio Cabral</h2>
+                 <xsl:apply-templates/> 
+            </body>
+        </html>
+    </xsl:template>
+    
+    <xsl:template match="act">
+    <p style="font-size: 30px; margin-left: 50"><xsl:apply-templates /></p>
+     <br/>
+    </xsl:template>
+
+    <xsl:template match="scene">
+    <p style="font-size: 25px; margin-left: 50"><xsl:apply-templates /></p>
+     <br/>
+    </xsl:template>
+
+    <xsl:template match="speech">
+    <p style="margin-left:100"><xsl:apply-templates /></p>
+    </xsl:template>
+
+    <xsl:template match="stage">
+    <p style="font-style: italic"><xsl:apply-templates /></p>
+    </xsl:template>
+
+    <xsl:template match="speaker">
+    <p style="font-weight:bold"><xsl:apply-templates /></p>
+    </xsl:template>
+ 
+</xsl:stylesheet>
